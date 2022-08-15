ae662738b4d2c7b9ca24cde011a64b223982d0ba
brendanburke <bfburke33@gmail.com>
2022-08-15 12:46:00 -0400

    fixed component and pages files,  added react router to index

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Routes } from 'react-router';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router >
+      <Routes>
+        <Route path='/' element={<App />} ></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
